01e78b9e321060d618250601ac58d83e72c1901b
spdskatr <25078279+spdskatr@users.noreply.github.com>
2024-01-25 19:09:18 +0000

    Mess with cmake to generate bitcode for rtlib

--- gpu-rtlib/RTlib.cpp
+++ gpu-rtlib/RTlib.cpp
@@ -11,12 +11,12 @@
 
 #define HIP_ASSERT(x) (assert((x)==hipSuccess))
 
-__device__ void __llvm_profile_register_names_function(void *s) {
-    printf("registered names %p\n", s);
+__global__ void __llvm_profile_register_names_function(void *s) {
+    //printf("registered names %p\n", s);
 }
 
 __global__ void __llvm_profile_register_function(void *s) {
-    printf("registered %p\n", s);
+    //printf("registered %p\n", s);
 }
 
 void test(void) {
