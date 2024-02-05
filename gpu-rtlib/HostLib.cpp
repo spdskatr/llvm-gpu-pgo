9b93f651ff91fd6b85f8b742d5edc52a81869cb0
spdskatr <25078279+spdskatr@users.noreply.github.com>
2024-02-05 12:29:43 +0000

    Add dummy hostlib code

--- gpu-rtlib/HostLib.cpp
+++ gpu-rtlib/HostLib.cpp
@@ -1,3 +1,16 @@
 #include "RTLib.h"
+#include <cstdio>
+#include <cstdlib>
 
-ProfDataLocs *__llvm_gpuprof_loc = nullptr;
+ProfDataLocs *__init_loc(void);
+
+ProfDataLocs *__llvm_gpuprof_loc = __init_loc();
+
+void __dump_loc(void) {
+    printf("Location: %p\n", __llvm_gpuprof_loc);
+}
+
+ProfDataLocs *__init_loc(void) {
+    atexit(__dump_loc);
+    return nullptr;
+}
