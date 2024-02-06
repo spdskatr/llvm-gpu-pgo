e17e73aad1e3846c9743ce5abbd5d0cb84f28fd6
spdskatr <25078279+spdskatr@users.noreply.github.com>
2024-02-06 16:29:27 +0000

    Add proxy buffer for beaming back data

--- gpu-rtlib/HostLib.cpp
+++ gpu-rtlib/HostLib.cpp
@@ -10,6 +10,17 @@ ProfDataLocs *init_loc(void);
 
 ProfDataLocs *__llvm_gpuprof_loc = init_loc();
 
+// Some GPUs don't like you directly copying from arbitrary memory locations
+// back into the host. We create a proxy buffer on the device and perform two
+// memcpys instead. 
+void memcpyArbitraryDeviceToHost(void *hostPtr, const void *devPtr, size_t size) {
+    void *proxyPtr;
+    HIP_ASSERT(hipMalloc(&proxyPtr, size));
+    HIP_ASSERT(hipMemcpy(proxyPtr, devPtr, size, hipMemcpyDeviceToDevice));
+    HIP_ASSERT(hipMemcpy(hostPtr, proxyPtr, size, hipMemcpyDeviceToHost));
+    HIP_ASSERT(hipFree(proxyPtr));
+}
+
 void dump_res(ProfDataLocs& res) {
     printf("DataFirst: %p\n", res.DataFirst);
     printf("DataLast: %p\n", res.DataLast);
@@ -17,25 +28,21 @@ void dump_res(ProfDataLocs& res) {
     printf("NamesLast: %p\n", res.NamesLast);
     printf("CountersFirst: %p\n", res.CountersFirst);
     printf("CountersLast: %p\n", res.CountersLast);
-    size_t dataSize = (res.DataLast - res.DataFirst) * sizeof(__llvm_profile_data);
-    char *counters = (char *)malloc(dataSize);
+    size_t countersSize = (res.CountersLast - res.CountersFirst) * sizeof(char);
+    char *counters = (char *)malloc(countersSize);
     if (!counters) {
         fprintf(stderr, "wtf!!!\n");
         return;
     }
 
-    hipError_t err = hipMemcpy(counters, res.DataFirst, dataSize, hipMemcpyDeviceToHost);
-    if (err != hipSuccess) {
-        puts(hipGetErrorName(err));
-    } else {
-        printf("Data:");
-        for (size_t i = 0; i < dataSize; i++) {
-            printf(" %02x", (unsigned char)counters[i]);
-        }
-        printf("\n");
+    memcpyArbitraryDeviceToHost(counters, res.CountersFirst, countersSize);
+
+    printf("counters:");
+    for (size_t i = 0; i < countersSize; i++) {
+        printf(" %02x", (unsigned char)counters[i]);
     }
+    printf("\n");
     free(counters);
-    
 }
 
 extern "C"
