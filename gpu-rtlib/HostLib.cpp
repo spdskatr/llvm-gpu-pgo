a8978c8ca99fef1df10d169349c9fa5a209ce377
spdskatr <25078279+spdskatr@users.noreply.github.com>
2024-02-07 15:15:50 +0000

    Implement beaming back all data

--- gpu-rtlib/HostLib.cpp
+++ gpu-rtlib/HostLib.cpp
@@ -8,7 +8,14 @@
 #define HIP_ASSERT(x) (assert((x)==hipSuccess))
 ProfDataLocs *init_loc(void);
 
+// Define the host symbol that gets registered with DeviceLib symbol by the
+// GPUInstrPass.
 ProfDataLocs *__llvm_gpuprof_loc = init_loc();
+
+// Device addresses for profiling data.
+ProfDataLocs DeviceLoc;
+// Host addresses for profiling data.
+ProfDataLocs HostLoc;
 
 // Some GPUs don't like you directly copying from arbitrary memory locations
 // back into the host. We create a proxy buffer on the device and perform two
@@ -21,34 +28,61 @@ void memcpyArbitraryDeviceToHost(void *hostPtr, const void *devPtr, size_t size)
     HIP_ASSERT(hipFree(proxyPtr));
 }
 
-void dump_res(ProfDataLocs& res) {
+void dumpRes(ProfDataLocs& res) {
     printf("DataFirst: %p\n", res.DataFirst);
     printf("DataLast: %p\n", res.DataLast);
     printf("NamesFirst: %p\n", res.NamesFirst);
     printf("NamesLast: %p\n", res.NamesLast);
     printf("CountersFirst: %p\n", res.CountersFirst);
     printf("CountersLast: %p\n", res.CountersLast);
-    size_t countersSize = (res.CountersLast - res.CountersFirst) * sizeof(char);
-    char *counters = (char *)malloc(countersSize);
+}
 
-    memcpyArbitraryDeviceToHost(counters, res.CountersFirst, countersSize);
+template <typename T>
+size_t distanceInBytes(T* start, T* end) {
+    return (end - start) * sizeof(T);
+}
 
-    printf("counters:");
-    for (size_t i = 0; i < countersSize; i++) {
-        printf(" %02x", (unsigned char)counters[i]);
-    }
-    printf("\n");
-    free(counters);
+void fetchLocs() {
+    HIP_ASSERT(hipMemcpyFromSymbol(&DeviceLoc, __llvm_gpuprof_loc,
+        sizeof(ProfDataLocs), 0, hipMemcpyDeviceToHost));
+    // Also allocate memory for HostLoc
+    size_t DataSize = DeviceLoc.DataLast - DeviceLoc.DataFirst;
+    size_t NamesSize = DeviceLoc.NamesLast - DeviceLoc.NamesFirst;
+    size_t CountersSize = DeviceLoc.CountersLast - DeviceLoc.CountersFirst;
+
+    HostLoc.DataFirst = (__llvm_profile_data *)malloc(DataSize * sizeof(__llvm_profile_data));
+    HostLoc.DataLast = HostLoc.DataFirst + DataSize;
+    HostLoc.NamesFirst = (const char *)malloc(NamesSize * sizeof(char));
+    HostLoc.NamesLast = HostLoc.NamesFirst + DataSize;
+    HostLoc.CountersFirst = (char *)malloc(CountersSize * sizeof(char));
+    HostLoc.CountersLast = HostLoc.CountersFirst + CountersSize;
+}
+
+void fetchData() {
+    assert(HostLoc.DataFirst && "Locs were not fetched!");
+    memcpyArbitraryDeviceToHost(
+        (void *)HostLoc.DataFirst, (void *)DeviceLoc.DataFirst,
+        distanceInBytes(HostLoc.DataFirst, HostLoc.DataLast));
+    memcpyArbitraryDeviceToHost(
+        (void *)HostLoc.NamesFirst, (void *)DeviceLoc.NamesFirst,
+        distanceInBytes(HostLoc.NamesFirst, HostLoc.NamesLast));
+    memcpyArbitraryDeviceToHost(
+        (void *)HostLoc.CountersFirst, (void *)DeviceLoc.CountersFirst,
+        distanceInBytes(HostLoc.CountersFirst, HostLoc.CountersLast));
 }
 
 extern "C"
 void __llvm_gpuprof_sync(void) {
     HIP_ASSERT(hipDeviceSynchronize());
-    ProfDataLocs hostLoc = {};
-
-    HIP_ASSERT(hipMemcpyFromSymbol(&hostLoc, __llvm_gpuprof_loc,
-        sizeof(ProfDataLocs), 0, hipMemcpyDeviceToHost));
-    dump_res(hostLoc);
+    if (!DeviceLoc.DataFirst)
+        fetchLocs();
+    fetchData();
+    dumpRes(DeviceLoc);
+    printf("counters:");
+    for (char *c = HostLoc.CountersFirst; c < HostLoc.CountersLast; c++) {
+        printf(" %02x", (unsigned char)*c);
+    }
+    printf("\n");
 }
 
 ProfDataLocs *init_loc(void) {
