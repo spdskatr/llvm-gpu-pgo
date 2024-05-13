3d78af96e9387b03fc57673abda80007c79a142f
spdskatr <25078279+spdskatr@users.noreply.github.com>
2024-05-13 08:08:53 +0100

    Scrub crsid

--- tests/lit.cfg.py
+++ tests/lit.cfg.py
@@ -9,6 +9,6 @@ config.test_format = lit.formats.ShTest("0")
 config.suffixes = {".ll", ".cpp"}
 
 config.environment["HIP_PATH"] = os.environ.get("HIP_PATH") or "/opt/rocm"
-config.environment["LLVM_ROOT"] = os.environ.get("LLVM_ROOT") or "/home/ts862/llvm17/build"
-config.environment["GPUPGO_ROOT"] = os.environ.get("GPUPGO_ROOT") or "/home/ts862/llvm-gpu-pgo"
+config.environment["LLVM_ROOT"] = os.environ.get("LLVM_ROOT") or os.environ.get("HOME") + "/llvm17/build"
+config.environment["GPUPGO_ROOT"] = os.environ.get("GPUPGO_ROOT") or os.environ.get("HOME") + "/llvm-gpu-pgo"
 config.environment["HIP_CLANG_PATH"] = os.path.join(config.environment["LLVM_ROOT"], "bin")
